d4b625d80a30953fb792f134bb81486b55ac3a6d
Ayham Al Ali <alali_ayham@yahoo.com>
2021-03-07 17:54:25 +0200

    Remove HEX code regex
    Removed HEX code regex since at this point it will be already parsed into &x format

--- src/main/java/ch/njol/skript/util/chat/ChatMessages.java
+++ src/main/java/ch/njol/skript/util/chat/ChatMessages.java
@@ -43,67 +43,68 @@ import net.md_5.bungee.api.ChatColor;
  * Handles parsing chat messages.
  */
 public class ChatMessages {
-	
+
 	/**
 	 * Link parse mode for potential links which are not marked with tags.
 	 */
 	public static LinkParseMode linkParseMode = LinkParseMode.DISABLED;
-	
+
 	/**
 	 * If color codes should also function as reset code.
 	 */
 	public static boolean colorResetCodes = false;
-	
+
 	/**
 	 * Chat codes, see {@link SkriptChatCode}.
 	 */
 	static final Map<String, ChatCode> codes = new HashMap<>();
-	
+
 	/**
 	 * Chat codes registered by addons, as backup for language changes.
 	 */
 	static final Set<ChatCode> addonCodes = new HashSet<>();
-	
+
 	/**
 	 * Color chars, as used by Mojang's legacy chat colors.
 	 */
 	static final ChatCode[] colorChars = new ChatCode[256];
-	
+
 	/**
 	 * Used to detect links when strict link parsing is enabled.
 	 */
 	@SuppressWarnings("null")
-	static final Pattern linkPattern = Pattern.compile("[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)");
-	
+	static final Pattern linkPattern = Pattern
+			.compile("[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)");
+
 	/**
 	 * Instance of GSON we use for serialization.
 	 */
 	static final Gson gson;
-	
+
 	/**
-	 * Registers language change listener for chat system.
-	 * Called once by Skript, please don't call this addon developers.
+	 * Registers language change listener for chat system. Called once by Skript,
+	 * please don't call this addon developers.
 	 */
 	public static void registerListeners() {
 		// When language changes or server is loaded loop through all chatcodes
 		Language.addListener(new LanguageChangeListener() {
-			
+
 			@Override
 			public void onLanguageChange() {
 				codes.clear();
-				
+
 				Skript.debug("Parsing message style lang files");
 				for (SkriptChatCode code : SkriptChatCode.values()) {
 					assert code != null;
 					registerChatCode(code);
 				}
-				
+
 				// Re-register any missing addon chat codes
 				for (ChatCode code : addonCodes) {
 					assert code != null;
 					registerChatCode(code);
 				}
-				
+
 				// Add formatting chars
 				addColorChar('k', SkriptChatCode.obfuscated);
 				addColorChar('l', SkriptChatCode.bold);
@@ -114,10 +115,10 @@ public class ChatMessages {
 			}
 		});
 	}
-	
+
 	static void registerChatCode(ChatCode code) {
 		String langName = code.getLangName();
-		
+
 		if (code.isLocalized()) {
 			if (code.getColorCode() != null) { // Color code!
 				// Avoid dependency on SkriptColor
@@ -132,33 +133,34 @@ public class ChatMessages {
 		} else { // Not localized, lang name is as-is
 			codes.put(langName, code);
 		}
-		
+
 		// Register color char
 		if (code.getColorChar() != 0) {
 			addColorChar(code.getColorChar(), code);
 		}
 	}
-	
+
 	static void addColorChar(char code, ChatCode data) {
 		colorChars[code] = data;
 		colorChars[Character.toUpperCase(code)] = data;
 	}
-	
+
 	static {
-		Gson nullableGson = new GsonBuilder().registerTypeAdapter(boolean.class, new MessageComponent.BooleanSerializer()).create();
+		Gson nullableGson = new GsonBuilder()
+				.registerTypeAdapter(boolean.class, new MessageComponent.BooleanSerializer()).create();
 		assert nullableGson != null;
 		gson = nullableGson;
 	}
-	
+
 	/**
 	 * Component list - this is just serialization mapper class.
 	 */
 	private static class ComponentList {
-		
+
 		public ComponentList(List<MessageComponent> components) {
 			this.extra = components;
 		}
-		
+
 		@SuppressWarnings("unused")
 		public ComponentList(MessageComponent[] components) {
 			this.extra = Arrays.asList(components);
@@ -170,7 +172,7 @@ public class ChatMessages {
 		@SuppressWarnings("unused")
 		@Deprecated
 		public String text = "";
-		
+
 		/**
 		 * Actual message data.
 		 */
@@ -178,26 +180,27 @@ public class ChatMessages {
 		@Nullable
 		public List<MessageComponent> extra;
 	}
-	
+
 	/**
 	 * Parses a string to list of chat message components.
+	 * 
 	 * @param msg Input string.
 	 * @return List with components.
 	 */
 	@SuppressWarnings("null")
 	public static List<MessageComponent> parse(String msg) {
 		char[] chars = msg.toCharArray();
-		
+
 		List<MessageComponent> components = new ArrayList<>();
 		MessageComponent current = new MessageComponent();
 		components.add(current);
 		StringBuilder curStr = new StringBuilder();
-		
+
 		for (int i = 0; i < chars.length; i++) {
 			char c = chars[i];
 			ChatCode code = null;
 			String param = "";
-			
+
 			if (c == '<') { // Tag parsing
 				// Find where the tag ends
 				int end = -1;
@@ -208,13 +211,13 @@ public class ChatMessages {
 						angleBrackets++;
 					else if (c2 == '>')
 						angleBrackets--;
-					
+
 					if (angleBrackets == 0) {
 						end = j;
 						break;
 					}
 				}
-				
+
 				if (end != -1) { // If this COULD be valid tag...
 					String tag = msg.substring(i + 1, end);
 					String name;
@@ -226,26 +229,26 @@ public class ChatMessages {
 						name = tag;
 					}
 					name = name.toLowerCase(); // Tags are case-insensitive
-					
+
 					boolean tryHex = Utils.HEX_SUPPORTED && name.startsWith("#");
 					ChatColor chatColor = null;
 					if (tryHex) {
 						chatColor = Utils.parseHexColor(name);
 						tryHex = chatColor != null;
 					}
-					
+
 					code = codes.get(name);
 					if (code != null || tryHex) { // ... and if the tag IS really valid
 						String text = curStr.toString();
 						curStr = new StringBuilder();
 						assert text != null;
 						current.text = text;
-						
+
 						MessageComponent old = current;
 						current = new MessageComponent();
-						
+
 						components.add(current);
-						
+
 						if (tryHex) {
 							current.color = chatColor;
 						} else if (code.getColorCode() != null) { // Just update color code
@@ -254,16 +257,17 @@ public class ChatMessages {
 							assert param != null;
 							code.updateComponent(current, param); // Call SkriptChatCode update
 						}
-						
+
 						// Copy styles from old to current if needed
 						copyStyles(old, current);
-						
+
 						// Increment i to tag end
 						i = end;
 						continue;
 					}
-					
-					// If this is invalid tag, just ignore it. Maybe scripter was trying to be clever with formatting
+
+					// If this is invalid tag, just ignore it. Maybe scripter was trying to be
+					// clever with formatting
 				}
 			} else if (c == '&' || c == '§') {
 				// Corner case: this is last character, so we cannot get next
@@ -271,16 +275,16 @@ public class ChatMessages {
 					curStr.append(c);
 					continue;
 				}
-				
+
 				char color = chars[i + 1];
-				
+
 				boolean tryHex = Utils.HEX_SUPPORTED && color == 'x';
 				ChatColor chatColor = null;
 				if (tryHex && i + 14 < chars.length) { // Try to parse hex "&x&1&2&3&4&5&6"
 					chatColor = Utils.parseHexColor(msg.substring(i + 2, i + 14).replace("&", "").replace("§", ""));
 					tryHex = chatColor != null;
 				}
-				
+
 				if (color >= colorChars.length) { // Invalid Unicode color character
 					curStr.append(c);
 					continue;
@@ -293,12 +297,12 @@ public class ChatMessages {
 					curStr = new StringBuilder();
 					assert text != null;
 					current.text = text;
-					
+
 					MessageComponent old = current;
 					current = new MessageComponent();
-					
+
 					components.add(current);
-					
+
 					if (tryHex) { // Set color to hex ChatColor
 						current.color = chatColor;
 						i = i + 12; // Skip past all the tags
@@ -307,24 +311,24 @@ public class ChatMessages {
 					} else {
 						code.updateComponent(current, param); // Call SkriptChatCode update
 					}
-					
+
 					// Copy styles from old to current if needed
 					copyStyles(old, current);
 				}
-				
+
 				i++; // Skip this and color char
 				continue;
 			}
-			
+
 			// Attempt link parsing, if a tag was not found
 			if (linkParseMode == LinkParseMode.STRICT && c == 'h') {
 				String rest = msg.substring(i); // Get rest of string
-				
+
 				String link = null;
 				if (rest.startsWith("http://") || rest.startsWith("https://")) {
 					link = rest.split(" ", 2)[0];
 				}
-				
+
 				// Link found
 				if (link != null && !link.isEmpty()) {
 					// Take previous component, create new
@@ -332,19 +336,19 @@ public class ChatMessages {
 					curStr = new StringBuilder();
 					assert text != null;
 					current.text = text;
-					
+
 					MessageComponent old = current;
 					current = new MessageComponent();
 					copyStyles(old, current);
-					
+
 					components.add(current);
-					
+
 					// Make new component a link
 					SkriptChatCode.open_url.updateComponent(current, link); // URL for client...
 					current.text = link; // ... and for player
-					
+
 					i += link.length() - 1; // Skip link for all other parsing
-					
+
 					// Add one MORE component (this comes after the link)
 					current = new MessageComponent();
 					components.add(current);
@@ -353,13 +357,13 @@ public class ChatMessages {
 			} else if (linkParseMode == LinkParseMode.LENIENT && (i == 0 || chars[i - 1] == ' ')) {
 				// Lenient link parsing
 				String rest = msg.substring(i); // Get rest of string
-				
+
 				String link = null;
 				String potentialLink = rest.split(" ", 2)[0]; // Split stuff
 				if (linkPattern.matcher(potentialLink).matches()) { // Check if it is at least somewhat valid URL
 					link = potentialLink;
 				}
-				
+
 				// Link found
 				if (link != null && !link.isEmpty()) {
 					// Insert protocol (aka guess it) if it isn't there
@@ -369,71 +373,73 @@ public class ChatMessages {
 					} else {
 						url = link;
 					}
-					
+
 					// Take previous component, create new
 					String text = curStr.toString();
 					curStr = new StringBuilder();
 					assert text != null;
 					current.text = text;
-					
+
 					MessageComponent old = current;
 					current = new MessageComponent();
 					copyStyles(old, current);
-					
+
 					components.add(current);
-					
+
 					// Make new component a link
 					SkriptChatCode.open_url.updateComponent(current, url); // URL for client...
 					current.text = link; // ... and for player
-					
+
 					i += link.length() - 1; // Skip link for all other parsing
-					
+
 					// Add one MORE component (this comes after the link)
 					current = new MessageComponent();
 					components.add(current);
 					continue;
 				}
 			}
-				
+
 			curStr.append(c); // Append this char to curStr
 		}
-		
+
 		String text = curStr.toString();
 		assert text != null;
 		current.text = text;
-		
+
 		return components;
 	}
-	
+
 	@SuppressWarnings("null")
 	public static MessageComponent[] parseToArray(String msg) {
 		return parse(msg).toArray(new MessageComponent[0]);
 	}
-	
+
 	public static String toJson(String msg) {
 		ComponentList componentList = new ComponentList(parse(msg));
 		String json = gson.toJson(componentList);
 		assert json != null;
 		return json;
 	}
-	
+
 	public static String toJson(List<MessageComponent> components) {
 		ComponentList componentList = new ComponentList(components);
 		String json = gson.toJson(componentList);
 		assert json != null;
 		return json;
 	}
-	
+
 	/**
 	 * Copies styles from component to another. Note that this only copies
-	 * additional styling, i.e. if text was not bold and is bold, it will remain bold.
+	 * additional styling, i.e. if text was not bold and is bold, it will remain
+	 * bold.
+	 * 
 	 * @param from
 	 * @param to
 	 */
 	public static void copyStyles(MessageComponent from, MessageComponent to) {
 		if (to.reset)
 			return;
-		
+
 		// If we don't have color or colors don't reset formatting, copy formatting
 		if (to.color == null || !colorResetCodes) {
 			if (!to.bold)
@@ -449,8 +455,9 @@ public class ChatMessages {
 			if (to.color == null)
 				to.color = from.color;
 		}
-		
-		// Links and such are never reset by color codes - weird, but it'd break too much stuff
+
+		// Links and such are never reset by color codes - weird, but it'd break too
+		// much stuff
 		if (to.clickEvent == null)
 			to.clickEvent = from.clickEvent;
 		if (to.insertion == null)
@@ -472,6 +479,7 @@ public class ChatMessages {
 
 	/**
 	 * Constructs plain text only message component.
+	 * 
 	 * @param str
 	 */
 	public static MessageComponent plainText(String str) {
@@ -479,22 +487,24 @@ public class ChatMessages {
 		component.text = str;
 		return component;
 	}
-	
+
 	/**
 	 * Registers a chat code. This is for addon developers.
-	 * @param code Something that implements {@link ChatCode}.
-	 * For inspiration, check {@link SkriptChatCode} source code.
+	 * 
+	 * @param code Something that implements {@link ChatCode}. For inspiration,
+	 *             check {@link SkriptChatCode} source code.
 	 */
 	public static void registerAddonCode(@Nullable SkriptAddon addon, @Nullable ChatCode code) {
 		Objects.requireNonNull(addon);
 		Objects.requireNonNull(code);
-		
+
 		addonCodes.add(code); // So that language reloads don't break everything
 		registerChatCode(code);
 	}
-	
+
 	/**
 	 * Strips all styles from given string.
+	 * 
 	 * @param text String to strip styles from.
 	 * @return A string without styles.
 	 */
@@ -505,18 +515,17 @@ public class ChatMessages {
 			sb.append(component.text);
 		}
 		String plain = sb.toString();
-		
-		// To be extra safe, strip valid <, >, §, & and hex codes <#123456>; protects against bugs in parser
-		if (Utils.HEX_SUPPORTED) // Strip '§x'
-			plain = plain.replace("§x", "");
-		/*
-			1st Replace: strips json
-			2nd Replace: strips colors using <color name> (Note: this regex expr repalce any two words surrounded by <> such as <any text>)
-			3rd Replace: strips colors & or §
-			4th Replace: strips colors using hex codes
-		*/
-		plain = plain.replaceAll("<(link|url|run command|suggest command|tooltip):(.*?)>", "$2").replaceAll("<\\w+? ?\\w+?>", "").replaceAll("(?i)[&§][0-9a-folkrnm]", "").replaceAll("(?i)<#[0-9a-z]{6}>", "");
 
+		// To be extra safe, strip valid <, >, §, &; protects against bugs in parser
+		if (Utils.HEX_SUPPORTED) // Strip '§x', '&x'
+			plain = plain.replace("[§&]x", "");
+		/*
+		 * 1st Replace: strips json 2nd Replace: strips colors using <color name> (Note:
+		 * this regex expr repalce any two words surrounded by <> such as <any text>)
+		 * 3rd Replace: strips colors & or § 4th Replace: strips colors using hex codes
+		 */
+		plain = plain.replaceAll("<(link|url|run command|suggest command|tooltip):(.*?)>", "$2")
+				.replaceAll("<\\w+? ?\\w+?>", "").replaceAll("(?i)[&§][0-9a-folkrnm]", "");
 
 		assert plain != null;
 		return plain;
