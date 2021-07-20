7cb406101063cfd2f1dc2a63c92bcb0c33683dd4
Ayham Alali <alali_ayham@yahoo.com>
2021-07-20 13:00:50 +0300

    Fixes

--- src/main/java/ch/njol/skript/conditions/CondHasLineOfSight.java
+++ src/main/java/ch/njol/skript/conditions/CondHasLineOfSight.java
@@ -19,14 +19,16 @@
 package ch.njol.skript.conditions;
 
 import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.conditions.base.PropertyCondition.PropertyType;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Condition;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
-import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
@@ -34,40 +36,41 @@ import org.eclipse.jdt.annotation.Nullable;
 @Name("Has Line of Sight")
 @Description("Checks whether the living entity has block line of sight to another living entity.")
 @Examples({"target entity has line of sight on player",
+			"",
 			"loop all players:",
-			"\tplayer has sight on location of targeted block of loop-player # Checks if another player is looking at the same location",
-			"\tsend \"%loop-player% is looking at the same block as yours\" to player"})
+			"\tloop-player is not player # Filter player",
+			"\tloop-player has sight on player # Checks if another player is looking at player",
+			"\tsend \"%loop-player% is looking at you!\" to player"})
 @Since("INSERT VERSION")
-public class CondHasLineOfSight extends PropertyCondition<LivingEntity> {
+public class CondHasLineOfSight extends Condition {
 	
 	static {
-		register(CondHasLineOfSight.class, PropertyType.HAVE, "([line of] sight|view) on %livingentity/location%", "livingentities");
+		PropertyCondition.register(CondHasLineOfSight.class, PropertyType.HAVE, "([line of] sight|view) on %entity%", "livingentities");
 	}
 	
-	private Expression<?> target;
+	private Expression<LivingEntity> entity;
+	private Expression<Entity> target;
 
 	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		target = exprs[1];
-		return super.init(exprs, matchedPattern, isDelayed, parseResult);
+	public boolean init(Expression[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		entity = (Expression<LivingEntity>) exprs[0];
+		target = (Expression<Entity>) exprs[1];
+		setNegated(matchedPattern == 1);
+		return true;
 	}
 
 	@Override
-	public boolean check(LivingEntity entity) {
-		if (target instanceof Location)
-			return entity.hasLineOfSight((Location) target);
-		else
-			return entity.hasLineOfSight((LivingEntity) target);
-	}
-
-	@Override
-	protected String getPropertyName() {
-		return "line of sight";
+	public boolean check(Event e) {
+		Entity target = this.target.getSingle(e);
+		if (target == null)
+			return false;
+		
+		return entity.check(e, ev -> ev.hasLineOfSight(target), isNegated());
 	}
 
 	@Override
 	public String toString(@Nullable Event e, boolean debug) {
-		return PropertyCondition.toString(this, PropertyType.HAVE, e, debug, getExpr(), "line of sight on " + target.toString(e, debug));
+		return PropertyCondition.toString(this, PropertyType.HAVE, e, debug, entity, "line of sight on " + target.toString(e, debug));
 	}
 	
 }
